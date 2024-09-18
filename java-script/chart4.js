dbea0ce8e4eddc693d94bdae9e45f65b492292b6
Andreicomia11-12 <dreibanaag13@gmail.conm>
2024-09-19 07:56:28 +0800

    Frontend Proj

---
+++ java-script/chart4.js
@@ -0,0 +1,21 @@
+const ctcc = document.getElementById('myChart4');
+
+  new Chart(ctcc, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+ 
